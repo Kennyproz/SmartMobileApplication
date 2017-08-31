9a14b1cefca822cd2e5b6de346fac662225df793
Ken <k.odigwegomez@student.fontys.nl>
2017-08-31 15:25:45 +0200

    Added a button function

--- app/src/main/java/com/example/ken/smartmobileapplication/MainActivity.java
+++ app/src/main/java/com/example/ken/smartmobileapplication/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.ken.smartmobileapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    int i = 0;
+
+    protected void clicked(View view){
+        System.out.println("Op de knop geklikt");
+        System.out.println(i);
+        i++;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+
 }
