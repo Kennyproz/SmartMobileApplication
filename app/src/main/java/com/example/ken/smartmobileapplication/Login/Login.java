e190af95940489e70f5950188a25620af6c7aa1d
Ken <k.odigwegomez@student.fontys.nl>
2017-09-21 15:12:37 +0200

    Added SharedPreferences to save login credentials.

--- app/src/main/java/com/example/ken/smartmobileapplication/Login/Login.java
+++ app/src/main/java/com/example/ken/smartmobileapplication/Login/Login.java
@@ -1,17 +1,16 @@
 package com.example.ken.smartmobileapplication.Login;
 
+import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.ken.smartmobileapplication.BasicAppPOC.MainActivity;
-import com.example.ken.smartmobileapplication.BasicAppPOC.SecondActivity;
 import com.example.ken.smartmobileapplication.R;
 
 import java.util.ArrayList;
@@ -20,49 +19,66 @@ import java.util.List;
 public class Login extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.ken.smartmobileapplication.MESSAGE";
+
     List<User> users;
     boolean isLoggedIn;
+    EditText un,ps;
+    String username,password;
+    CheckBox chkbox;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        users = new ArrayList<User>();
+
         addUsers();
+        getIds();
         isLoggedIn = false;
+
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        String uname = sharedPreferences.getString("Username", null);
+        String pass = sharedPreferences.getString("Password",null);
+
+        checkString(uname,un);
+        checkString(pass,ps);
+    }
+
+    private void checkString(String string, EditText editText){
+        if (string != null){
+            editText.setText(string);
+        }
+        else {
+            return;
+        }
     }
 
     public void register(View v){
         Intent intent = new Intent(this,Register.class);
+        intent.putExtra("users", (ArrayList<User>)users);
         startActivity(intent);
     }
 
     public void login(View v)
     {
-        EditText un = (EditText)findViewById(R.id.tbUsername);
-        EditText ps = (EditText)findViewById(R.id.tbPassword);
-        String username = un.getText().toString();
-        String password = ps.getText().toString();
-       if (!username.isEmpty() && !password.isEmpty()){
+        getIds();
+        getInlogStrings();
+        if (!username.isEmpty() && !password.isEmpty()){
             for (User u : users){
               if(u.login(username,password)){
                   Intent intent = new Intent(this, Home.class);
                   String textMessage = username;
                   intent.putExtra(EXTRA_MESSAGE,textMessage);
-
-                  startActivity(intent);
                   isLoggedIn = true;
+                  saveCredentials();
+                  startActivity(intent);
                   break;
               }
             }
-           if (isLoggedIn) {
-               Toast.makeText(Login.this,"Logged in!",Toast.LENGTH_LONG).show();
-           }
-           else {
-               Toast.makeText(Login.this,"Login Failed!",Toast.LENGTH_LONG).show();
-           }
-
-
+           isLoggedIn();
        }
        else {
            Toast.makeText(Login.this,"Username or password is empty!",Toast.LENGTH_LONG).show();
@@ -70,18 +86,40 @@ public class Login extends AppCompatActivity {
     }
 
     private void addUsers(){
+        users = new ArrayList<User>();
         users.add(new User("Ken","Ken","test","Kenny@Hotmail.com"));
         users.add(new User("Marcel","Marcel","test","Marcel@Hotmail.com"));
         users.add(new User("Arjan","Arjan","test","Arjan@Hotmail.com"));
         users.add(new User("Ken","Kennyboy","test","Kenny@Hotmail.com"));
     }
     private void saveCredentials(){
-        CheckBox credentials = (CheckBox)findViewById(R.id.chkCredent);
-        if (credentials.isActivated()){
-
+        if (chkbox.isChecked()){
+            editor.putString("Username",username);
+            editor.putString("Password",password);
+            editor.commit();
         }
         else {
-            return;
+            editor.clear().commit();
+        }
+    }
+
+    private void getIds(){
+        un = (EditText)findViewById(R.id.tbUsername);
+        ps = (EditText)findViewById(R.id.tbPassword);
+        chkbox = (CheckBox)findViewById(R.id.chkCredent);
+    }
+
+    private void getInlogStrings(){
+        username = un.getText().toString();
+        password = ps.getText().toString();
+    }
+
+    private void isLoggedIn(){
+        if (isLoggedIn) {
+            Toast.makeText(Login.this,"Logged in!",Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(Login.this,"Login Failed!",Toast.LENGTH_LONG).show();
         }
     }
 }
--- app/src/main/java/com/example/ken/smartmobileapplication/Login/Register.java
+++ app/src/main/java/com/example/ken/smartmobileapplication/Login/Register.java
@@ -1,11 +1,22 @@
 package com.example.ken.smartmobileapplication.Login;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import com.example.ken.smartmobileapplication.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Register extends AppCompatActivity {
+
+    List<User> userList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +24,51 @@ public class Register extends AppCompatActivity {
         setContentView(R.layout.activity_layout_register);
 
         Intent intent = getIntent();
+        userList = (ArrayList<User>) intent.getSerializableExtra("users");
+    }
 
+    public void registerUser(View v){
+        EditText username = (EditText)findViewById(R.id.editTextUsername);
+        for (User u : userList){
+            if (u.getUsername().toString().equals(username.getText().toString())){
+                Toast.makeText(Register.this,"Username already in Use!!",Toast.LENGTH_LONG).show();
+            }
+            else{
+                addUser();
+                //Intent intent = new Intent(this,Login.class);
+                //intent.putExtra("allUsers", (ArrayList<User>)userList);
+                //startActivity(intent);
+                //Toast.makeText(Register.this,"The user is registered succesfully!",Toast.LENGTH_LONG).show();
+                break;
+            }
+        }
+    }
+    private void addUser(){
+        EditText edUsername = (EditText)findViewById(R.id.editTextUsername);
+        EditText edName = (EditText)findViewById(R.id.editTextName);
+        EditText edPass1 = (EditText)findViewById(R.id.editTextPass);
+        EditText edPass2 = (EditText)findViewById(R.id.editTextpass);
+        EditText edEmail = (EditText)findViewById(R.id.editTextEmail);
+
+        String username = edUsername.getText().toString();
+        String name = edName.getText().toString();
+        String pass = edPass1.getText().toString();
+        String pass1 = edPass2.getText().toString();
+        String mail = edEmail.getText().toString();
+
+        if (pass.equals(pass1) && !pass.isEmpty()){
+            if (username.isEmpty() || name.isEmpty() || mail.isEmpty())
+            {
+                Toast.makeText(Register.this,"Please fill in all fields!",Toast.LENGTH_LONG).show();
+            }
+            else {
+                User user = new User(name,username,pass,mail);
+                userList.add(user);
+            }
+        }
+        else {
+            Toast.makeText(Register.this,"Password do not match!!",Toast.LENGTH_LONG).show();
+        }
     }
 
 
